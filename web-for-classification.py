766c1369e201e7ae08ddb87ecb22412fd814a77a
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 22:26:25 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -7,11 +7,21 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
-model = EfficientNetB0(weights='imagenet')
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return EfficientNetB0(weights='imagenet')
+
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Оберіть зображення')
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -20,16 +30,20 @@ def load_image():
         return None
 
 
-st.title('Класифікація зображень')
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+
+model = load_model()
+
+
+st.title('Новая улучшенная классификации изображений в облаке Streamlit')
 img = load_image()
-
-result = st.button('Розпізнати зображення')
-
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-
-preds = model.predict(x)
-
-classes = decode_predictions(preds, top=3)[0]
-for cl in classes:
-    st.write(cl[1], cl[2])
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
