d47fae14118235a870e7591d2ca3538d474ab57f
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 19:19:23 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -1,25 +1,15 @@
 import io
-
 import streamlit as st
-
 from PIL import Image
 
 def load_image():
-
-uploaded_file = st.file_uploader(label='Оберіть зображення')
-
-if uploaded_file is not None:
-
-image_data = uploaded_file.getvalue()
-
-st.image(image_data)
-
-return Image.open(io.BytesIO(image_data))
-
-else:
-
-return None
+    uploaded_file = st.file_uploader(label='Оберіть зображення')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
 
 st.title('Класифікація зображень')
-
 img = load_image()
