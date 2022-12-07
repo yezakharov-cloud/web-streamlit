c58657250f40b9ae5eb9a44ba8d039e116c8eed6
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 20:33:46 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -2,13 +2,6 @@ import io
 import streamlit as st
 from PIL import Image
 
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input
-from tensorflow.keras.applications.efficientnet import decode_predictions
-import numpy as np
-
-
 def load_image():
     uploaded_file = st.file_uploader(label='Оберіть зображення')
     if uploaded_file is not None:
