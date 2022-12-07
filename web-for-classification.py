6684be32cb25a22faa2039b2b4117378a154cdee
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 22:23:16 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -25,7 +25,6 @@ img = load_image()
 
 result = st.button('Розпізнати зображення')
 
-img = img.resize((224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
