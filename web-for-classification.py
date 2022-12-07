0f1b0d0b075494304701bd10e9422268dd746fdd
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 21:22:59 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -7,10 +7,21 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return EfficientNetB0(weights='imagenet')
+
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Оберіть зображення')
+    uploaded_file = st.file_uploader(label='Виберіть зображення для розпізнавання')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -18,6 +29,21 @@ def load_image():
     else:
         return None
 
+
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+
+model = load_model()
+
+
 st.title('Класифікація зображень')
 img = load_image()
 result = st.button('Розпізнати зображення')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('Результати розпізнавання')
+    print_predictions(preds)
