df6f3d2d7e5ac40e3173da569738819a91b389b3
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 22:00:30 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -26,20 +26,17 @@ def load_image():
     else:
         return None
 
-def print_predictions(preds):
-    classes = decode_predictions(preds, top=3)[0]
-    for cl in classes:
-        st.write(cl[1], cl[2])
 
 
+    st.title('Класифікація зображень')
+    img = load_image()
+    result = st.button('Розпізнати зображення')
 
+    if result:
+        x = preprocess_image(img)
+        preds = model.predict(x)
+        st.write('**Результаты распознавания:**')
 
-st.title('Класифікація зображень')
-img = load_image()
-result = st.button('Розпізнати зображення')
-
-if result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
-    st.write('**Результаты распознавания:**')
-    print_predictions(preds)
+        classes = decode_predictions(preds, top=3)[0]
+        for cl in classes:
+       st.write(cl[1], cl[2])
