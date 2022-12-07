9f814f814a5e6f2cbd2239220588c2ae8bac2b48
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 22:11:47 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -9,13 +9,6 @@ from tensorflow.keras.applications.efficientnet import preprocess_input, decode_
 
 model = EfficientNetB0(weights='imagenet')
 
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
-
 
 def load_image():
     uploaded_file = st.file_uploader(label='Оберіть зображення')
@@ -27,10 +20,13 @@ def load_image():
         return None
 
 
-
 st.title('Класифікація зображень')
 img = load_image()
 result = st.button('Розпізнати зображення')
+
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
 
 preds = model.predict(x)
 
