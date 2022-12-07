d0dbfdf1a832db0b5085a8a8bb44560a605fd7f8
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 20:36:35 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -2,7 +2,8 @@ import io
 import streamlit as st
 from PIL import Image
 
-pip install --upgrade tf_agents
+import tensorflow_probability as tfp
+
 
 def load_image():
     uploaded_file = st.file_uploader(label='Оберіть зображення')
