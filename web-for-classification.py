b857e3bb2869c45cdd930f0dcb852ad928d9b5ed
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 21:19:17 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -1,9 +1,10 @@
 import io
 import streamlit as st
-
-
-
 from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
 
