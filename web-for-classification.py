581713602744b6a43ec099e81215646b139623f8
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 20:42:25 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -1,34 +1,10 @@
-"altair>=3.2.0",
-"blinker>=1.0.0",
-"cachetools>=4.0",
-"click>=7.0",
-# 1.4 introduced the functionality found in python 3.8's importlib.metadata module
-"importlib-metadata>=1.4",
-"numpy",
-"packaging>=14.1",
-"pandas>=0.21.0",
-"pillow>=6.2.0",
-"protobuf<4,>=3.12",
-"pyarrow>=4.0",
-"pympler>=0.9",
-"python-dateutil",
-"requests>=2.4",
-"rich>=10.11.0",
-"semver",
-"toml",
-"typing-extensions>=3.10.0.0",
-"tzlocal>=1.1",
-"validators>=0.2",
-
-
-
-
+pip install --upgrade pip
 
 import io
 import streamlit as st
 from PIL import Image
 
-import tensorflow_probability as tfp
+
 
 
 def load_image():
