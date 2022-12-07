8d22ee0678809dd74cd9312aebaa4e9e2972252f
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2022-12-07 21:26:56 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -11,17 +11,11 @@ from tensorflow.keras.applications.efficientnet import preprocess_input, decode_
 def load_model():
     return EfficientNetB0(weights='imagenet')
 
-
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
+model = EfficientNetB0(weights='imagenet')
 
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Виберіть зображення для розпізнавання')
+    uploaded_file = st.file_uploader(label='Оберіть зображення')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -29,21 +23,24 @@ def load_image():
     else:
         return None
 
-
-def print_predictions(preds):
-    classes = decode_predictions(preds, top=3)[0]
-    for cl in classes:
-        st.write(cl[1], cl[2])
-
-
-model = load_model()
-
-
 st.title('Класифікація зображень')
 img = load_image()
-result = st.button('Розпізнати зображення')
-if result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
-    st.write('Результати розпізнавання')
-    print_predictions(preds)
+
+img = image.load_img(img_path, target_size=(224, 224))
+
+plt.imshow(img)
+plt.show()
+
+
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+
+preds = model.predict(x)
+
+
+
+classes = decode_predictions(preds, top=3)[0]
+for cl in classes:
+    print(cl[1], cl[2])
